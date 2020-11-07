5130dbf8722640dd86b33023f789b28e4c8a8ac3
Ira Tyschenko <tyschenk40@gmail.com>
2020-11-07 13:56:38 +0200

    added repository interface

---
+++ FootballProject.Dal.Abstract/Repositories/IFootballerRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using FootballProject.Entities;
+
+namespace FootballProject.Dal.Abstract.Repositories
+{
+    public interface IFootballerRepository<in TKey> where TKey: struct
+    {
+        IEnumerable<Footballer> GetFootballers();
+        IEnumerable<Footballer> GetFootballersBySearch(string search);
+        Footballer GetFootballerById(TKey footballerId);
+        bool FootballerExists(TKey footballerId);
+    }
+}
