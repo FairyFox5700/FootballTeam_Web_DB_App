63a1ca0e26d64f6001eb5017053949d901b54b90
Ira Tyschenko <tyschenk40@gmail.com>
2020-11-11 19:40:26 +0200

    added  sponsores repository

---
+++ FootballProject.Dal.Abstract/Repositories/ISponsoresRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FootballProject.Entities;
+
+namespace FootballProject.Dal.Abstract.Repositories
+{
+    public interface ISponsoresRepository<in TKey> where TKey: struct
+    {
+
+        Task<IEnumerable<Sponsor>> GetSponsoresById(int sponsorId);
+        Task<Sponsor> GetSponsorById(int sponsorId);
+         
+    }
+}
---
+++ FootballProject.Dal.Impl/Repositories/SponsoresRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using FootballProject.Dal.Abstract.Repositories;
+using FootballProject.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace FootballProject.Dal.Impl.Repositories
+{
+    public class SponsoresRepository:ISponsoresRepository<int>
+    {
+        private string _connectionString;
+
+        public SponsoresRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        public async Task<IEnumerable<Sponsor>> GetSponsoresById(int clubId)
+        {
+            var query = @"EXEC public.get_all_sponsores_by_club_id @clubId  = @ClubId";
+            await using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            return await connection.QueryAsync<Sponsor>(
+                query,
+                param: new
+                {
+                    ClubId=clubId
+                });
+        }
+
+        public async Task<Sponsor> GetSponsorById(int sponsorId)
+        {
+            var query = @"EXEC public.get_sponore_by_id @sponsoreId = @SponsorId";
+            await using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            var result = await connection.QueryAsync<Sponsor>(
+                query,
+                param: new
+                {
+                    SponsorId= sponsorId
+                });
+            return result.FirstOrDefault();
+        }
+    }
+}
